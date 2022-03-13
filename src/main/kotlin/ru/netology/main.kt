ca7a1da3d3681940c6a15f0ef3c7977d5ef075d2
Dilyara44 <shafigullina.di@gmail.com>
2022-03-13 22:53:26 +0300

    Initial Commit

---
+++ src/main/kotlin/ru/netology/main.kt
@@ -0,0 +1,11 @@
+package ru.netology
+
+fun main() {
+    val sum = 1345.toDouble()
+    val sumOfCommission = 0.75
+    val minCommission = 35.toDouble()
+
+    val commission = sum * sumOfCommission / 100
+    val amount = if (commission < minCommission) sum - 35 else sum - commission
+    println ("Вы хотите перевести: $sum руб. После комиссии сумма составит: $amount руб.")
+}
