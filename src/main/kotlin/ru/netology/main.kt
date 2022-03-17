c69edf62b5fbb64f328514c2518842ecfc5099d1
Dilyara44 <100514175+Dilyara44@users.noreply.github.com>
2022-03-17 16:03:44 +0300

    Update main.kt

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -1,9 +1,9 @@
 package ru.netology
 
 fun main() {
-    val sum = 1345.toDouble()
+    val sum = 1345
     val sumOfCommission = 0.75
-    val minCommission = 35.toDouble()
+    val minCommission = 35
 
     val commission = sum * sumOfCommission / 100
     val amount = if (commission < minCommission) sum - minCommission else sum - commission
